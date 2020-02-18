23838dbd16d949a2802242ac730c1277be98e91c
Caesarexe <61145077+Caesarexe@users.noreply.github.com>
2020-02-18 11:24:01 +0100

    Version 1.0 of my application.

---
+++ src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'findLanguageFromKey' })
+export class FindLanguageFromKeyPipe implements PipeTransform {
+  private languages: { [key: string]: { name: string; rtl?: boolean } } = {
+    en: { name: 'English' },
+    de: { name: 'Deutsch' },
+    ja: { name: '日本語' },
+    ko: { name: '한국어' },
+    ru: { name: 'Русский' }
+    // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
+  };
+
+  transform(lang: string): string {
+    return this.languages[lang].name;
+  }
+}
